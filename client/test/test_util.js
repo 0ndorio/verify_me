829b3ed804fea42f236a80d73280f8f042ca1bb6
Bruno Kirschner <bruno.kirschner@online.de>
2015-10-24 15:33:08 +0200

    ClientTests: adds test cases for #bigInt2ByteString()

--- client/test/test_util.js
+++ client/test/test_util.js
@@ -9,17 +9,29 @@ describe("util", function() {
   beforeEach(function() {});
   afterEach(function() {});
 
-  describe("#bigInt2ByteString", function(){
-
-    it("should return '0' when BigInt is 0" , function(){
-
-      var bigInt = util.bigIntFromInt(1111);
-
-      var result = util.bigInt2ByteString(bigInt);
-      assert.equal("\u0004W", result);
-    });
+  describe("#bigInt2ByteString()", function() {
 
     var tests = [
+      {arg: 1111, expected: "\u0004W"},
+      {arg: 256,  expected: "\u0001\u0000"},
+      {arg: 1,  expected: "\u0001"},
+      {arg: 0,  expected: "\u0000"},
+      {arg: -1,  expected: "ÿÿÿ"},
+      {arg: -2,  expected: "ÿÿþ"},
+      {arg: -256,  expected: "ÿÿ\u0000"},
+      {arg: -1111,  expected: "ÿû©"}
+    ];
+
+    tests.forEach(function(test) {
+      it("should return the byte string '" + escape(test.expected) + "' when input is BigInt with " + test.arg , function(){
+        var bigInt = util.bigIntFromInt(test.arg);
+        var result = util.bigInt2ByteString(bigInt);
+
+        assert.equal(test.expected, result);
+      });
+    });
+
+    tests = [
       {arg: "123", expected: null},
       {arg: 123,   expected: null},
       {arg: true,  expected: null},
