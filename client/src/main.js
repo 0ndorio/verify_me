2ac048b11273314e82138fb785d65614165c5d74
Bruno Kirschner <bruno.kirschner@online.de>
2015-11-04 15:12:34 +0100

    Client Main: restructure file and imporved error descriptions and TODOs

--- client/src/main.js
+++ client/src/main.js
@@ -4,28 +4,19 @@ var blinding = require("./blinding");
 var client = require("./client");
 var util = require("./util");
 
-document.getElementById("activate_pseudonym_button").onclick = requestPseudonym;
+function checkResult(unblinded_message, blinding_information) {
 
-/// TODO
-function requestPseudonym()
-{
-  var blinding_information = client.collectPublicBlindingInformation();
+  console.log('Signed Message:');
+  console.log('---------------');
+  console.log(util.bigInt2ByteString(unblinded_message)+'\n\n');
 
-  util.generatePrimeNumber(1024).then(function(prime) {
+  var e = blinding_information.public_exponent;
+  var N = blinding_information.modulus;
+  var m = unblinded_message.modPow(e, N);
 
-    var token = client.getToken();
-    blinding_information.blinding_factor = token.data.multiply(prime);
-    blinding_information.hashed_token = util.bytes2hex(util.hashMessage(token.data.toRadix()));
-
-    var public_key_string = client.getPublicKeyString();
-    var hashed_message = util.hashMessage(public_key_string);
-
-    return blinding.blind_message(hashed_message, blinding_information);
-  })
-  .then(function(blinded_message) {
-
-    return serverRequest(blinded_message, blinding_information);
-  })
+  console.log('Original Message:');
+  console.log('-----------------');
+  console.log(util.bigInt2ByteString(m));
 }
 
 /// Dummy for a synchronous xmlhttprequest
@@ -50,17 +41,41 @@ function serverRequest(blinded_message, blinding_information)
   }));
 }
 
-function checkResult(unblinded_message, blinding_information) {
+/// TODO
+function requestPseudonym()
+{
+  var blinding_information = client.collectPublicBlindingInformation();
 
-  console.log('Signed Message:');
-  console.log('---------------');
-  console.log(util.bigInt2ByteString(unblinded_message)+'\n\n');
+  /// blinding factor and modulus must be coprime
+  ///   - ensured through the use of prime numbers
+  ///   - blinding factor is generated through the multiplication of prime token and two additional prime numbers
+  /// blinding factor must be smaller than Modulus
+  ///   - number of bits after multiplication of two numbers with the same bit length (n) is 2n or less
+  ///     (https://math.stackexchange.com/questions/682618/the-maximum-number-of-digits-in-binary-multiplication)
+  ///   - TODO: unsure how to handle this properly
+  var prime_bit_length = Math.floor(blinding_information.modulus.bitLength / 4);
 
-  var e = blinding_information.public_exponent;
-  var N = blinding_information.modulus;
-  var m = unblinded_message.modPow(e, N);
+  util.generatePrimeNumbers(prime_bit_length).then(function(primes) {
 
-  console.log('Original Message:');
-  console.log('-----------------');
-  console.log(util.bigInt2ByteString(m));
+    var public_key_string = client.getPublicKeyString();
+    var hashed_message = util.hashMessage(public_key_string);
+
+    var token = client.getToken().byteLength();
+
+    blinding_information.blinding_factor = token.data.multiply(primes[0].multiply(primes[1]));
+
+    /// TODO: workaround for unhandled length problem ... if blinding factor is to large this reduces its size
+    if (blinding_information.blinding_factor.compareTo(blinding_information.modulus) > 0) {
+      blinding_information.blinding_factor = token.data.multiply(primes[0]);
+    }
+
+    blinding_information.hashed_token = util.bytes2hex(util.hashMessage(token.data.toRadix()));
+
+    return blinding.blind_message(hashed_message, blinding_information);
+  })
+  .then(function(blinded_message) {
+    return serverRequest(blinded_message, blinding_information);
+  });
 }
+
+document.getElementById("activate_pseudonym_button").onclick = requestPseudonym;
