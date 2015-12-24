4d18c916b9fa9e9ee70845e5d1569e01baed87bf
Bruno Kirschner <bruno.kirschner@online.de>
2015-12-24 15:43:49 +0100

    Client: - replaces function based types {BlindSignature, BlindingInformation} with es6 classes

--- client/src/types/blind_signature_packet.js
+++ client/src/types/blind_signature_packet.js
@@ -11,146 +11,150 @@ import * as util from "../util"
 /// TODO
 /// @parameter {KeyManager} target_key
 /// @parameter {KeyManager} sig_key
-let BlindSignaturePacket = function(target_key, sig_key)
+export default class BlindSignaturePacket extends sig.Signature
 {
-  this.target_key = target_key;
-  this.primary = target_key.pgp.key(target_key.pgp.primary);
+  constructor(target_key, sig_key)
+  {
+    const hashed_subpackets = [
+      new sig.CreationTime(BlindSignaturePacket.calculateRandomCreationDate(target_key)),
+      new sig.PreferredHashAlgorithms([Constants.openpgp.hash_algorithms.SHA512])
+    ];
 
-  const hashed_subpackets = [
-    new sig.CreationTime(this.calculateRandomCreationDate(target_key)),
-    new sig.PreferredHashAlgorithms([Constants.openpgp.hash_algorithms.SHA512])
-  ];
-
-  const unhashed_subpackets = [
+    const unhashed_subpackets = [
       new sig.Issuer(sig_key.get_pgp_key_id())
-  ];
+    ];
 
-  const ctor_args = {
-    hashed_subpackets: hashed_subpackets,
-    key: sig_key.get_primary_keypair(),
-    key_id: sig_key.get_pgp_key_id(),
-    unhashed_subpackets: unhashed_subpackets,
-    type: Constants.openpgp.sig_types.persona,
-    version: Constants.openpgp.versions.signature.V4
-  };
+    const ctor_args = {
+      hashed_subpackets: hashed_subpackets,
+      key: sig_key.get_primary_keypair(),
+      key_id: sig_key.get_pgp_key_id(),
+      unhashed_subpackets: unhashed_subpackets,
+      type: Constants.openpgp.sig_types.persona,
+      version: Constants.openpgp.versions.signature.V4
+    };
 
-  sig.Signature.call(this, ctor_args);
-  this.prepare_raw_sig();
-};
+    super(ctor_args);
 
-BlindSignaturePacket.prototype = Object.create(sig.Signature.prototype);
-BlindSignaturePacket.prototype.tag = kbpgp.const.openpgp.packet_tags.signature;
+    this.tag = kbpgp.const.openpgp.packet_tags.signature;
+    this.target_key = target_key;
+    this.primary = target_key.pgp.key(target_key.pgp.primary);
 
-/// Create random creation time between key creation und expire date
-/// @parameter {KeyManager} target_public_key
-BlindSignaturePacket.prototype.calculateRandomCreationDate = function(target_key)
-{
-  if (!util.isOpenPGPKey(target_key)) {
-    return false;
+    this.prepare_raw_sig();
   }
 
-  const lifespan = target_key.primary.lifespan;
-  let key_expire = lifespan.expire_in;
+  /// Create random creation time between key creation und expire date
+  /// @parameter {KeyManager} target_public_key
+  static calculateRandomCreationDate(target_key)
+  {
+    if (!util.isOpenPGPKey(target_key)) {
+      return false;
+    }
 
-  // if no expire date is available use maximum possible day in the future
-  //    - 100,000,000 days measured relative to midnight at the beginning of 01 January, 1970 UTC.
-  //      http://es5.github.io/#x15.9.1.1
-  if (null === key_expire || 0 >= key_expire) {
-    key_expire = 8640000000000000;
+    const lifespan = target_key.primary.lifespan;
+    let key_expire = lifespan.expire_in;
+
+    // if no expire date is available use maximum possible day in the future
+    //    - 100,000,000 days measured relative to midnight at the beginning of 01 January, 1970 UTC.
+    //      http://es5.github.io/#x15.9.1.1
+    if (null === key_expire || 0 >= key_expire) {
+      key_expire = 8640000000000000;
+    }
+
+    return Math.floor(lifespan.generated + Math.random() * (key_expire - lifespan.generated));
   }
 
-  return Math.floor(lifespan.generated + Math.random() * (key_expire - lifespan.generated));
-};
+  /// TODO
+  prepare_raw_sig()
+  {
+    // TODO: Input Checks
 
-/// TODO
-BlindSignaturePacket.prototype.prepare_raw_sig = function()
-{
-  // TODO: Input Checks
+    const signData = this.generate_sig_payload();
+    const hashed_signData = this.hasher(signData);
+    const target_length = this.key.pub.n.mpi_byte_length();
 
-  const signData = this.generate_sig_payload();
-  const hashed_signData = this.hasher(signData);
-  const target_length = this.key.pub.n.mpi_byte_length();
+    this.raw_signature = pad.emsa_pkcs1_encode(
+      hashed_signData, target_length, { hasher: this.hasher }
+    );
 
-  this.raw_signature = pad.emsa_pkcs1_encode(
-    hashed_signData, target_length, { hasher: this.hasher }
-  );
+    this.signed_hash_value_hash = this.raw_signature.toBuffer().slice(0, 2);
+  }
 
-  this.signed_hash_value_hash = this.raw_signature.toBuffer().slice(0, 2);
-};
+  /// TODO
+  generate_sig_payload()
+  {
+    const key_material_packet = this.target_key.pgp.key(this.target_key.pgp.primary);
+    const pubKeyData = key_material_packet.to_signature_payload();
 
-BlindSignaturePacket.prototype.generate_sig_payload = function()
-{
-  const key_material_packet = this.target_key.pgp.key(this.target_key.pgp.primary);
-  const pubKeyData = key_material_packet.to_signature_payload();
+    const user_id_packet = this.target_key.get_userids_mark_primary()[0];
+    const userIdData = user_id_packet.to_signature_payload();
 
-  const user_id_packet = this.target_key.get_userids_mark_primary()[0];
-  const userIdData = user_id_packet.to_signature_payload();
+    return kbpgp.Buffer.concat([
+      pubKeyData, userIdData, this.generate_sig_data()
+    ]);
+  }
 
-  return kbpgp.Buffer.concat([
-    pubKeyData, userIdData, this.generate_sig_data()
-  ]);
-};
+  /// TODO
+  generate_sig_data()
+  {
+    const sigHashData = this.generate_sig_prefix();
+    const sigTrailer = this.generate_sig_trailer(sigHashData.length);
 
-BlindSignaturePacket.prototype.generate_sig_data = function()
-{
-  const sigHashData = this.generate_sig_prefix();
-  const sigTrailer = this.generate_sig_trailer(sigHashData.length);
+    return Buffer.concat([sigHashData, sigTrailer]);
+  }
 
-  return Buffer.concat([sigHashData, sigTrailer]);
-};
+  /// TODO
+  generate_sig_trailer(hash_data_length)
+  {
+    return kbpgp.Buffer.concat([
+      new kbpgp.Buffer([
+        this.version,
+        0xff
+      ]),
+      kbpgp.util.uint_to_buffer(32, hash_data_length)
+    ]);
+  }
 
-BlindSignaturePacket.prototype.generate_sig_trailer = function(hash_data_length)
-{
-  return kbpgp.Buffer.concat([
-    new kbpgp.Buffer([
-      this.version,
-      0xff
-    ]),
-    kbpgp.util.uint_to_buffer(32, hash_data_length)
-  ]);
-};
+  /// TODO
+  generate_sig_prefix()
+  {
+    const hashedSubpkts = this.hashed_subpackets
+      .reduce((lhs, rhs) => kbpgp.Buffer.concat([lhs.to_buffer(), rhs.to_buffer()]));
 
-BlindSignaturePacket.prototype.generate_sig_prefix = function()
-{
-  const hashedSubpkts = this.hashed_subpackets
-    .reduce((lhs, rhs) => kbpgp.Buffer.concat([lhs.to_buffer(), rhs.to_buffer()]));
+    return kbpgp.Buffer.concat([
+      new Buffer([
+        this.version,
+        this.type,
+        this.key.type,
+        this.hasher.type
+      ]),
+      kbpgp.util.uint_to_buffer(16, hashedSubpkts.length),
+      hashedSubpkts
+    ]);
+  }
 
-  return kbpgp.Buffer.concat([
-    new Buffer([
-      this.version,
-      this.type,
-      this.key.type,
-      this.hasher.type
-    ]),
-    kbpgp.util.uint_to_buffer(16, hashedSubpkts.length),
-    hashedSubpkts
-  ]);
-};
+  /// Calculate & inject framed signature body
+  write()
+  {
+    const unframed_sig = this.write_unframed();
+    this._framed_output = this.frame_packet(this.tag, unframed_sig);
 
-/// Calculate & Inject Framed Signature Body
-BlindSignaturePacket.prototype.write = function()
-{
-  const unframed_sig = this.write_unframed();
-  this._framed_output = this.frame_packet(this.tag, unframed_sig);
+    return this._framed_output;
+  }
 
-  return this._framed_output;
-};
+  /// Calculate unframed signature body
+  write_unframed()
+  {
+    const unhashed_packet_data = this.unhashed_subpackets.reduce(
+      (prevValue, subpacket) => { return kbpgp.Buffer.concat([prevValue, subpacket.to_buffer()])},
+      new kbpgp.Buffer({})
+    );
 
-/// Calculate Unframed Signature Body
-BlindSignaturePacket.prototype.write_unframed = function()
-{
-  const unhashed_packet_data = this.unhashed_subpackets.reduce(
-    (prevValue, subpacket) => { return kbpgp.Buffer.concat([prevValue, subpacket.to_buffer()])},
-    new kbpgp.Buffer({})
-  );
-
-  return kbpgp.Buffer.concat([
-    this.generate_sig_prefix(),
-    kbpgp.util.uint_to_buffer(16, unhashed_packet_data.length),
-    unhashed_packet_data,
-    this.signed_hash_value_hash,
-    this.sig
-  ]);
-};
-
-module.exports = BlindSignaturePacket;
+    return kbpgp.Buffer.concat([
+      this.generate_sig_prefix(),
+      kbpgp.util.uint_to_buffer(16, unhashed_packet_data.length),
+      unhashed_packet_data,
+      this.signed_hash_value_hash,
+      this.sig
+    ]);
+  }
+}
--- client/src/types/blinding_information.js
+++ client/src/types/blinding_information.js
@@ -4,61 +4,61 @@ import * as util from "../util"
 
 /**
  * A rsa blinding context.
- * @constructor
- * @struct
  */
-function BlindingInformation()
+export default class BlindingInformation
 {
-  /** @type {BigInteger|null} */
-  this.blinding_factor = null;
-  /** @type {BigInteger|null} */
-  this.hashed_token = null;
-  /** @type {BigInteger|null} */
-  this.modulus = null;
-  /** @type {BigInteger|null} */
-  this.public_exponent = null;
-}
-
-/// TODO
-BlindingInformation.prototype.containsPublicBlindingInformation = function()
-{
-  return (util.isBigInteger(this.modulus) && util.isBigInteger(this.public_exponent));
-};
-
-/// TODO
-BlindingInformation.prototype.containsAllBlindingInformation = function()
-{
-  return this.containsPublicBlindingInformation()
-    && util.isBigInteger(this.blinding_factor)
-    && util.isBigInteger(this.hashed_token);
-};
-
-/// TODO
-BlindingInformation.prototype.fromKey = function(key)
-{
-  if (!util.isOpenPGPKey(key)) {
-    return false;
+  constructor()
+  {
+    /** @type {BigInteger|null} */
+    this.blinding_factor = null;
+    /** @type {BigInteger|null} */
+    this.hashed_token = null;
+    /** @type {BigInteger|null} */
+    this.modulus = null;
+    /** @type {BigInteger|null} */
+    this.public_exponent = null;
   }
 
-  const public_key_package = key.get_primary_keypair().pub;
-  this.modulus = public_key_package.n;
-  this.public_exponent = public_key_package.e;
+  /// TODO
+  containsPublicBlindingInformation()
+  {
+    return util.isBigInteger(this.modulus)
+        && util.isBigInteger(this.public_exponent);
+  }
 
-  return true;
-};
+  /// TODO
+  containsAllBlindingInformation()
+  {
+    return this.containsPublicBlindingInformation()
+        && util.isBigInteger(this.blinding_factor)
+        && util.isBigInteger(this.hashed_token);
+  }
 
-/// TODO
-BlindingInformation.isValidFullBlindingInformation = function(blinding_information)
-{
-  return (blinding_information instanceof BlindingInformation)
-    && blinding_information.containsAllBlindingInformation();
-};
+  /// TODO
+  fromKey(key)
+  {
+    if (!util.isOpenPGPKey(key)) {
+      return false;
+    }
 
-/// TODO
-BlindingInformation.isValidPublicBlindingInformation = function(blinding_information)
-{
-  return (blinding_information instanceof BlindingInformation)
-    && blinding_information.containsPublicBlindingInformation();
-};
+    const public_key_package = key.get_primary_keypair().pub;
+    this.modulus = public_key_package.n;
+    this.public_exponent = public_key_package.e;
 
-module.exports = BlindingInformation;
+    return true;
+  }
+
+  /// TODO
+  static isValidFullBlindingInformation(blinding_information)
+  {
+    return (blinding_information instanceof BlindingInformation)
+        && blinding_information.containsAllBlindingInformation();
+  }
+
+  /// TODO
+  static isValidPublicBlindingInformation(blinding_information)
+  {
+    return (blinding_information instanceof BlindingInformation)
+        && blinding_information.containsPublicBlindingInformation();
+  }
+}
