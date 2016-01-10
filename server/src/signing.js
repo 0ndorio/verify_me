7b3119f0331d7acda88ca47e64fcf9f4f2fa99e4
Bruno Kirschner <bruno.kirschner@online.de>
2016-01-11 00:43:33 +0100

    Server: adds ecdsa blind signature preparation

--- server/src/signing.js
+++ server/src/signing.js
@@ -5,7 +5,8 @@ import { BigInteger } from "../node_modules/kbpgp/lib/bn"
 
 import { rsa_promise, ecc_promise } from "./keys"
 
-export function sign_blinded_rsa_message(message, key_manager)
+/// TODO
+function sign_blinded_rsa_message(message, key_manager)
 {
   if (typeof message !== "string") {
     throw new Error("message is not of type string");
@@ -24,6 +25,51 @@ export function sign_blinded_rsa_message(message, key_manager)
   return m.modPow(d, n).toRadix(16);
 }
 
-export function sign_blinded_ecdsa_mesage(message, key_manager)
+/// TODO
+async function prepareEcdsaBlinding(key_manager)
 {
+  const public_key_package = key_manager.get_primary_keypair().pub;
+  const curve = public_key_package.curve;
+
+  const rx = BigInteger.ZERO;
+  do {
+
+    const k = await generateRandomScalar(curve);
+    const R = curve.G.multiply(k);
+    assert(curve.isOnCurve(R));
+
+  } while(0 === rx.compareTo(BigInteger.ZERO));
+
+  return {k, R};
 }
+
+/**
+ * Generate a random scalar k.
+ *
+ * k is in range [1, n-1] where n is the prime number defining
+ * the order of the givens curves base point.
+ *
+ * @param {Curve} curve
+ *    The curve we use to generate the random scalar value.
+ * @returns {Promise}
+ *    The promise of a {BigInteger} scalar [1, n-1]
+ */
+async function generateRandomScalar(curve)
+{
+  return new Promise((resolve, reject) =>
+    curve.random_scalar(
+      k => resolve(k))
+  );
+}
+
+/// TODO
+export function sign_blinded_ecdsa_message(message, key_manager)
+{}
+
+const signing_api = {
+  prepareEcdsaBlinding,
+  sign_blinded_rsa_message,
+  sign_blinded_ecdsa_message
+};
+
+export default signing_api;
