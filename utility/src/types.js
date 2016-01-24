2a9b89ebefac58f38d33062d93156f748708ec54
Bruno Kirschner <bruno.kirschner@online.de>
2016-01-24 19:37:30 +0100

    Utility: export custom kbpgp verification_algorithms constants

--- utility/src/types.js
+++ utility/src/types.js
@@ -6,7 +6,8 @@ export const Buffer = kbpgp.Buffer;
 export const Curve = kbpgp.ecc.curves.Curve;
 export const KeyManager = kbpgp.KeyManager;
 export const Tags = {
-  public_key_algorithms: kbpgp.const.openpgp.public_key_algorithms
+  public_key_algorithms: kbpgp.const.openpgp.public_key_algorithms,
+  verification_algorithms: kbpgp.const.openpgp.verification_algorithms
 };
 
 export { BigInteger } from "../node_modules/kbpgp/lib/bn"
